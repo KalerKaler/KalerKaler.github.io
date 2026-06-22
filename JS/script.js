2489745968e7d0affd80016767ff330cd631e310
KalerKaler <barjinderkalermp@gmail.com>
2026-06-22 18:41:32 +0530

    Submenu appears on hover instead of click now. Also, color correction

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,22 @@
 const menuBtn = document.getElementById("menu-btn");
 const submenu = document.getElementById("submenu");
 
-menuBtn.addEventListener('click', () => {
-    submenu.classList.toggle("active");
-})
+function showSubmenu() {
+    submenu.classList.add("show");
+}
+
+function hideSubmenu() {
+    setTimeout(() => {
+        isOverMenu = menuBtn.matches(":hover");
+        isOverSubmenu = submenu.matches(":hover");
+
+        if (!isOverMenu && !isOverSubmenu) {
+            submenu.classList.remove("show");
+        }
+    }, 100);
+}
+
+menuBtn.addEventListener("mouseover", showSubmenu);
+menuBtn.addEventListener("mouseout", hideSubmenu);
+submenu.addEventListener("mouseover", showSubmenu);
+submenu.addEventListener("mouseout", hideSubmenu);
